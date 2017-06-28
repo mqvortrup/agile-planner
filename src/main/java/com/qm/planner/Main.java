26b069659443228545dcce5a4ba020e4b3c09440
michael <qm@zuehlke.com>
2017-06-28 19:20:55 +0200

    Add main

---
+++ src/main/java/com/qm/planner/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2017. Michael Qvortrup
+ */
+
+package com.qm.planner;
+
+/**
+ * Created by michael on 6/28/2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello there");
+    }
+}
